ad7ed9cac869f3ae61fde29ca8012214bca05019
Murilo Lima <mglima.ntz@gmail.com>
2026-07-23 20:10:39 -0300

    .

--- inference.py
+++ inference.py
@@ -10,7 +10,7 @@ from model import UNet
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = UNet()
-checkpoint = torch.load("unet_mri_model.pth", map_location=device)
+checkpoint = torch.load("unet_mri_regularized.pth", map_location=device)
 model.load_state_dict(checkpoint['model_state_dict'])
 model.to(device)
 model.eval()
